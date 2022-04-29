7b893311a15ba3912fab110bdda0e9c7f2aedf80
IrvinAufs <irvin@aufs.io>
2022-04-29 11:53:28 +0800

    every user has default balance

--- contracts/AuroraToken.sol
+++ contracts/AuroraToken.sol
@@ -7,8 +7,12 @@ import "./ERC223/IERC223Recipient.sol";
 import "./utils/Address.sol";
 
 contract AuroraToken is IERC223, Ownable {
+
+    uint256 private _defaultAmount;
+
     constructor(uint256 amount) IERC223("AuroraFS Token", "AUFS") {
-        _mint(_msgSender(), amount);
+        setDefaultAmount(amount);
+        mint(_msgSender(), _defaultAmount);
     }
 
     function transfer(address to, uint amount, bytes calldata data) external override returns (bool) {
@@ -21,8 +25,60 @@ contract AuroraToken is IERC223, Ownable {
         return 9;
     }
 
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balanceOf(account, true);
+    }
+
+    function defaultAmount() public view returns (uint256) {
+        return _defaultAmount;
+    }
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function setDefaultAmount(uint256 amount) public onlyOwner {
+        _defaultAmount = amount;
+    }
+
+    function _balanceOf(address account, bool check) internal view returns (uint256 balances) {
+        if (!check)
+           return super.balanceOf(account);
+
+        balances = super.balanceOf(account);
+        if (balances == 0) {
+            return _defaultAmount;
+        }
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+        // skip _mint to call
+        if (from == address(0))
+            return;
+
+        // skip _burn to call
+        if (to == address(0))
+            return;
+
+        uint256 balances = _balanceOf(from, false);
+        if (balances == 0) {
+            _mint(from, _defaultAmount);
+        }
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override {
+        // skip _mint to call
+        if (from == address(0))
+            return;
+
+        // skip _burn to call
+        if (to == address(0))
+            return;
+
+        uint256 balances = _balanceOf(to, false);
+        if (balances == amount) {
+            _mint(to, _defaultAmount);
+        }
     }
 
     function _afterTokenTransfer(address from, address to, uint256 amount, bytes memory data) internal override {
---
+++ test/aufs_token.js
@@ -0,0 +1,42 @@
+const AuroraToken = artifacts.require('AuroraToken');
+const truffleAssert = require('truffle-assertions');
+
+contract("Aurora Token Test", (accounts) => {
+    const coinValue = web3.utils.toBN('1000000000');
+    const defaultAmount = coinValue * 1000;
+
+    it("always has default amounts", async () => {
+        const token = await AuroraToken.deployed();
+
+        let balance0 = await token.balanceOf(accounts[0]);
+        assert.ok(defaultAmount.toString() === balance0.toString(), "deployer balance not equal to " + defaultAmount.toString());
+
+        let balance1 = await token.balanceOf(accounts[1]);
+        assert.ok(balance1.toString() === defaultAmount.toString(), "account1 balance not equal to " + defaultAmount.toString());
+
+        let result = await token.transfer.sendTransaction(accounts[1], coinValue * 10);
+        truffleAssert.eventEmitted(result, "Transfer");
+        assert.ok(result.receipt.logs.length == 2);
+        assert.ok(result.receipt.logs.some((l) => {
+            return l.args.from == '0x0000000000000000000000000000000000000000' && l.args.to == accounts[1];
+        }));
+
+        balance0 = await token.balanceOf(accounts[0]);
+        assert.ok(balance0.toString() === (coinValue * 990).toString(), "deployer balance not equal to right value: 990");
+
+        balance1 = await token.balanceOf(accounts[1]);
+        assert.ok(balance1.toString() === (coinValue * 1010).toString(), "account1 balance not equal to right value: 1010");
+
+        await truffleAssert.reverts(token.transfer.sendTransaction(accounts[1], defaultAmount), "ERC20: transfer amount exceeds balance");
+
+        await truffleAssert.passes(token.transfer.sendTransaction(accounts[1], coinValue * 990));
+
+        balance0 = await token.balanceOf(accounts[0]);
+        assert.ok(balance0.toString() === defaultAmount.toString(), "deployer balance not equal to " + defaultAmount.toString());
+
+        balance1 = await token.balanceOf(accounts[1]);
+        assert.ok(balance1.toString() === (2 * defaultAmount).toString(), "accounts1 must have double defaultAmounts balance");
+
+        await truffleAssert.passes(token.transfer.sendTransaction(accounts[1], coinValue));
+    });
+});
